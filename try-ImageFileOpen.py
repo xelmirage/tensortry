1ea0ad9c5b6e99cbe53bb3dca8784ff3cf578eab
xelmirage-pc <xelmirage@outlook.com>
2017-08-01 22:47:52 +0800

    try TFRecord

--- try-ImageFileOpen.py
+++ try-ImageFileOpen.py
@@ -7,7 +7,8 @@ with tf.Session() as sess:
     # 我们要读三幅图片A.jpg, B.jpg, C.jpg
     filename = ['data/0005.jpg', 'data/0006.jpg', 'data/0007.jpg']
     # string_input_producer会产生一个文件名队列
-    filename_queue = tf.train.string_input_producer(filename, shuffle=True, num_epochs=5)
+    epochs=5
+    filename_queue = tf.train.string_input_producer(filename, shuffle=True, num_epochs=epochs)
     # reader从文件名队列中读数据。对应的方法是reader.read
     reader = tf.WholeFileReader()
     key, value = reader.read(filename_queue)
@@ -17,10 +18,14 @@ with tf.Session() as sess:
     threads = tf.train.start_queue_runners(sess=sess)
 
     i = 0
-    for i in range(0,15):
+    for i in range(0,epochs*len(filename)):
 
         # 获取图片数据并保存
-        image_data = sess.run(value)
-        with open('out/test_%d.jpg' % i, 'wb') as f:
-            f.write(image_data)
-    
+        try:
+            image_data = sess.run(value)
+            with open('out/test_%04d.jpg' % i, 'wb') as f:
+                f.write(image_data)
+        except ValueError:
+            print("No valid integer! Please try again ...")
+            break
+
